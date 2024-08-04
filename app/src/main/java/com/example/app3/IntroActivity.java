8c306260357f21022995a648c9f1aae55cff944d
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-08-04 18:18:32 +0530

    first commit

---
+++ app/src/main/java/com/example/app3/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.example.app3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app3.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends AppCompatActivity {
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        binding.startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
