24a8a037175e7597b12277a7f7effdee6959a4a1
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-08-05 16:11:50 +0530

    third commit

--- app/src/main/java/com/example/app3/Activity/WorkoutActivity.java
+++ app/src/main/java/com/example/app3/Activity/WorkoutActivity.java
@@ -4,38 +4,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.bumptech.glide.Glide;
 import com.example.app3.Adapter.LessonsAdapter;
 import com.example.app3.Domain.Workout;
-import com.example.app3.R;
+import com.example.app3.databinding.ActivityMainBinding; // Ensure this import is correct
 import com.example.app3.databinding.ActivityWorkoutBinding;
 
-public class WorkoutActivity extends AppCompatActivity {
+public class WorkoutActivity extends AppCompatActivity{
     ActivityWorkoutBinding binding;
     private Workout workout;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-         binding=ActivityWorkoutBinding.inflate(getLayoutInflater());
+        binding=ActivityWorkoutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getObject();
         setVariable();
-
     }
-
     private void getObject() {
         workout=(Workout) getIntent().getSerializableExtra("object");
     }
-
     private void setVariable() {
-        int resId=getResources().getIdentifier(workout.getPicPath(),"drewable",getPackageName());
+        int resId=getResources().getIdentifier(workout.getPicPath(),"drawable",getPackageName());
         Glide.with(this)
                 .load(resId)
                 .into(binding.pic);
@@ -46,12 +43,19 @@ public class WorkoutActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         binding.titleTxt.setText(workout.getTitle());
         binding.ExcersizeTxt.setText(workout.getLessions().size()+"Exercise");
         binding.KcalTxt.setText(workout.getKcal()+"Kcal");
         binding.duration.setText(workout.getDurationAll());
         binding.descriptionTxt.setText(workout.getDescription());
 
+        binding.view3.setLayoutManager(new LinearLayoutManager(WorkoutActivity.this,LinearLayoutManager.VERTICAL,false));
         binding.view3.setAdapter(new LessonsAdapter(workout.getLessions()));
     }
+
+
+
+
+
 }
